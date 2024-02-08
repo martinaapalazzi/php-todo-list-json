795aeec4841a1c808bca51bbf688a09f60310082
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-08 16:36:03 +0100

    done + bonus

--- backend/list.php
+++ backend/list.php
@@ -4,27 +4,15 @@
 */
 
 // Recupero il contenuto del file contenente i dati
-$jsonStringFromDatabases = file_get_contents('/backend/databases/list.json');
+$jsonStringFromDatabases = file_get_contents('./databases/list.json');
 
 // Trasformo la stringa in una struttura dati utilizzabile in PHP
 $list = json_decode($jsonStringFromDatabases, true); // true -> ti da un arrey associativo di array; false -> ti da un array di objects che non abbiamo ancora visto.
-
-$completedList = [];
-
-foreach ($list as $index => $singleToDo) {
-    if (
-        isset($_GET['toDo']) == false
-        ||
-        $_GET['toDo'] == ''
-    ) {
-        $completedList[] = $singleToDo;
-    }
-}
 
 // Dico al client che la risposta contiene un json
 header('Content-Type: application/json');
 
 // Rispondo con il json preso dal file
-echo json_encode($completedList);
+echo json_encode($list); // == echo $jsonStringFromDatabases
 
 ?>
