793cd4c89b8de65b14d7900d8e2b567fe924d8ee
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-09 10:45:02 +0100

    added some notes

--- backend/list.php
+++ backend/list.php
@@ -6,8 +6,15 @@
 // Recupero il contenuto del file contenente i dati
 $jsonStringFromDatabases = file_get_contents('./databases/list.json');
 
-// Trasformo la stringa in una struttura dati utilizzabile in PHP
+// Trasformo la stringa in una struttura dati utilizzabile in PHP, per effettuare una manipolazione di dati (ad esempio aggiungere una chiave, con un for per esempio)
 $list = json_decode($jsonStringFromDatabases, true); // true -> ti da un arrey associativo di array; false -> ti da un array di objects che non abbiamo ancora visto.
+
+/* for ($i = 0; $i < count($list); $i++) {
+    $list[$i]['used'] = rand(0, 10);
+} --> Ho aggiunto la chiave used */
+
+// Poi lo ritrasformo in stringa per stamparlo 
+// echo json_encode($list);
 
 // Dico al client che la risposta contiene un json
 header('Content-Type: application/json');
